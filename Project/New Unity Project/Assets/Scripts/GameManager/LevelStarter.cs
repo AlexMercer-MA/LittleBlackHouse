573465c858351d54b0680d3fa340ab3245582b3d
Alex <1005711499@qq.com>
2018-07-07 16:36:04 +0800

    修改场景

---
+++ Project/New Unity Project/Assets/Scripts/GameManager/LevelStarter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStarter : MonoBehaviour {
+
+    public playerWorld world;
+
+	// Use this for initialization
+	void OnEnable () 
+    {
+        if (world == playerWorld.worldA)
+        {
+            this.transform.position = new Vector3(5.0f, 0.0f, 0.0f);
+        }
+        else if(world == playerWorld.worldB)
+        {
+            this.transform.position = new Vector3(-5.0f, 0.0f, 0.0f);
+        }
+	}	
+}
+
+public enum playerWorld
+{
+    worldA,
+    worldB,
+}
